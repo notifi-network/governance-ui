1a7e9294fee90c60e1459f02d2e60ad407255d0a
Nathan Chung <105258726+Nathan-Notifi@users.noreply.github.com>
2022-05-09 20:56:30 -0700

    create notifi ui component in alerts panel

--- components/NotificationsSwitch/index.tsx
+++ components/NotificationsSwitch/index.tsx
@@ -160,6 +160,29 @@ export default function NotificationsSwitch() {
     )
   }
 
+  const handleUnsubscribe = useCallback(
+    async (source: Source) => {
+      try {
+        if (data?.alerts) {
+          const sourceId = source.id
+          const alertToDelete = data.alerts?.find((alert) =>
+            alert.sourceGroup.sources.find((source) => source.id === sourceId)
+          )
+
+          alertToDelete?.id &&
+            (await deleteAlert({
+              alertId: alertToDelete.id,
+              keepSourceGroup: true,
+              keepTargetGroup: true,
+            }))
+        }
+      } catch (e) {
+        throw new Error(e)
+      }
+    },
+    [data?.alerts, deleteAlert]
+  )
+
   const NotificationBox = ({
     channels,
     description,
@@ -182,21 +205,21 @@ export default function NotificationsSwitch() {
           <div>
             <p className="inline-block text-sm">{description}</p>
           </div>
-        </div>
 
-        <div className="flex w-full justify-center pt-3">
-          <Button
-            className="w-full"
-            onClick={() =>
-              setNotificationStore((state) => {
-                state.modalState = modalState
-              })
-            }
-          >
-            Use {formatName(name)}
-          </Button>
+          <div className="flex w-full justify-center pt-3">
+            <Button
+              className="w-full"
+              onClick={() =>
+                setNotificationStore((state) => {
+                  state.modalState = modalState
+                })
+              }
+            >
+              Use {formatName(name)}
+            </Button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   )
 
